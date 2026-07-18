089a7585cef8de18b417ccfecf25d906ad85c95b
AndreDaGOAT <155287773+AndreDaGOAT@users.noreply.github.com>
2026-07-18 15:16:10 -0500

    Configure server and build settings in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/JECS-Mobile-Wash-Pros/",
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
 });
